0cef65ccc2ce8c95063a48203e4835b0025be2d4
Arsenii Lialchenko <arsenii.lialchenko@ukg.com>
2023-02-18 11:13:55 +0200

    Commit1 for mobile3

--- src/main/java/pageObjects/navitePages/BudgetActivityPage.java
+++ src/main/java/pageObjects/navitePages/BudgetActivityPage.java
@@ -2,11 +2,13 @@ package pageObjects.navitePages;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.iOSXCUITFindBy;
 import org.openqa.selenium.WebElement;
 
 public class BudgetActivityPage extends NativePageObject {
 
     @AndroidFindBy(xpath = "//*[contains(@text,'BudgetActivity')]")
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeOther[@label='Budget']")
     private WebElement budgetActivity;
 
     public BudgetActivityPage(AppiumDriver driver) {
